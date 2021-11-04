1249ed34c7f30bfac6b22be1f4624f4504472563
WBill <wbill@WBills-MacBook-Pro.local>
2021-11-04 22:32:47 +0700

    Update Hearing UI

---
+++ android/app/src/main/java/com/hearingtest/HearingTonePlayActivity.java
@@ -0,0 +1,13 @@
+package com.hearingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HearingTonePlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hearing_tone_play);
+    }
+}
